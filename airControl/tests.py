6e903e94ab0281c127267042e4b0b81ea34ad163
daniloqb <daniloqb@gmail.com>
2016-05-25 20:20:31 -0300

    Codigos AOC adicionados para analise

--- airControl/tests.py
+++ airControl/tests.py
@@ -5,31 +5,24 @@ __author__ = 'tic'
 
 york = AirConditioner(mode='normal', temp='24', fan='auto', sweep='off')
 
-#york.setStatus('off')
+
 print york.getCode()
 
 print
 print york.getCode('bin')
 print york.getCode('hex')
+print york.info()
 
-
-led = LED(1);
-
-crtlLed = ControllerDeviceEthernet(led,'143.106.230.104',80);
-
-#controller = ControllerDeviceCOM(york,'/dev/ttyACM0',9600)
 #controller = ControllerDeviceEthernet(york,'143.106.230.104',80)
-crtlLed.execute()
-time.sleep(1)
 #controller.execute()
 
 
+controller = ControllerDeviceCOM(york,'/dev/ttyACM0',9600)
 
 
 time.sleep(3)
 
-led.status = 0
 
-crtlLed.execute()
+#crtlLed.execute()
 york.status = 'off'
-#controller.execute()
+controller.execute()
